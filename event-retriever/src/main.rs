6da9f8f7cd5fabd1123f8484cc75e85ca8ac3ec0
Ben Smith <bh2smith@gmail.com>
2023-09-27 09:24:17 +0200

    use dotenv to load DB_URL

--- event-retriever/src/main.rs
+++ event-retriever/src/main.rs
@@ -1,9 +1,9 @@
-use crate::db_reader::diesel::DieselClient;
-use crate::db_reader::DBClient;
-
+use crate::db_reader::{diesel::DieselClient, DBClient};
+use dotenv::dotenv;
 pub mod db_reader;
 
 fn main() {
+    dotenv().ok();
     let db_url = std::env::var("DB_URL").expect("Missing env var DB_URL");
     let mut pg_client = DieselClient::new(&db_url).expect("Failed to connect to DB");
     let block = 10_000_000i64;
