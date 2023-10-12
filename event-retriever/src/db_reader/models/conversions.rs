9a28f5eb75a81531a9c64635e62edb069e66c71d
Benjamin Smith <bh2smith@users.noreply.github.com>
2023-10-12 23:08:08 +0200

    Add data store and implement API methods for loading and saving different structures (#22)

--- event-retriever/src/db_reader/models/conversions.rs
+++ event-retriever/src/db_reader/models/conversions.rs
@@ -1,5 +1,5 @@
 use bigdecimal::{BigDecimal, Num};
-use ethers::types::U256;
+use ethers::types::{Address, U256};
 
 pub fn u256_from_big_decimal(value: &BigDecimal) -> U256 {
     U256::from_dec_str(&value.to_string()).expect("Invalid value")
@@ -7,4 +7,8 @@ pub fn u256_from_big_decimal(value: &BigDecimal) -> U256 {
 
 pub fn big_decimal_from_u256(value: &U256) -> BigDecimal {
     BigDecimal::from_str_radix(&value.to_string(), 10).unwrap()
+}
+
+pub fn address_to_vec(address: Address) -> Vec<u8> {
+    address.as_bytes().to_vec()
 }
