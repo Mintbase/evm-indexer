5857aed579b77e2647133e474c3c4ec36aaa2ee5
Benjamin Smith <bh2smith@users.noreply.github.com>
2023-10-30 10:33:25 +0100

    Retry (#65)
    EthClient Retry
    ---------
    
    Co-authored-by: Till <till.friesewinkel@googlemail.com>

--- eth/src/rpc.rs
+++ eth/src/rpc.rs
@@ -9,6 +9,7 @@ use ethers::{
 use std::{
     collections::{HashMap, HashSet},
     sync::Arc,
+    time::Duration,
 };
 
 abigen!(ERC721Metadata, "./src/abis/ERC721Metadata.json");
@@ -59,35 +60,71 @@ pub struct Client {
     provider: Arc<Provider<Http>>,
 }
 
-impl Client {
-    pub fn new(url: &str) -> Result<Self> {
-        Ok(Self {
-            provider: Arc::new(Provider::<Http>::try_from(url)?),
-        })
-    }
+#[async_trait::async_trait]
+trait RetryGet<T: Send> {
+    async fn try_get(&self) -> Result<T>;
 
-    pub async fn get_block(&self, block: u64) -> Result<Option<BlockData>> {
-        let response = self.provider.get_block(block).await?;
-        Ok(match response {
+    // so this shouldn't only retry from one provider, but from multiple, esp. at the top of the chain
+    async fn retry_get(&self, max_retries: u32, wait_secs: u64) -> Result<T> {
+        let mut retries = 0;
+        loop {
+            match self.try_get().await {
+                Ok(result) => return Ok(result),
+                Err(err) => {
+                    retries += 1;
+                    if retries >= max_retries {
+                        return Err(err);
+                    } else {
+                        tracing::debug!(
+                            "failed rpc request attempt {} - trying again in {} seconds",
+                            retries,
+                            wait_secs
+                        );
+                        tokio::time::sleep(Duration::from_secs(wait_secs)).await;
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct GetBlock {
+    provider: Arc<Provider<Http>>,
+    block: u64,
+}
+
+#[async_trait::async_trait]
+impl RetryGet<Option<BlockData>> for GetBlock {
+    async fn try_get(&self) -> Result<Option<BlockData>> {
+        let res: Option<ethers::types::Block<ethers::types::H256>> =
+            self.provider.get_block(self.block).await?;
+
+        Ok(match res {
             Some(ethers_block) => Some(BlockData {
-                /// Could also use client_response for this, but its optional.
-                number: block,
+                // Could also use client_response for this, but its optional.
+                number: self.block,
                 time: ethers_block.timestamp.as_u64(),
             }),
             None => None,
         })
     }
+}
 
+struct GetBlockReceipts {
+    provider: Arc<Provider<Http>>,
+    block: u64,
+    indices: HashSet<u64>,
+}
+
+#[async_trait::async_trait]
+impl RetryGet<HashMap<u64, TxDetails>> for GetBlockReceipts {
     // TODO (Cost Optimization): on the number of `indices`.
     //  Example: QuickNode API credits for
     //  - eth_getBlockReceipts                    is 59 while
     //  - eth_getTransactionByBlockNumberAndIndex is 2
     //  So when indices.len() < 59/2 its cheaper to get them individually.
-    pub async fn get_block_receipts(
-        &self,
-        block: u64,
-        indices: HashSet<u64>,
-    ) -> Result<HashMap<u64, TxDetails>> {
+    async fn try_get(&self) -> Result<HashMap<u64, TxDetails>> {
+        let block = self.block;
         // First try eth_getBlockReceipts
         // This method is only supported by a few node providers: Erigon (e.g. QuickNode, Alchemy, Ankr).
         // But not Infura for example
@@ -96,7 +133,7 @@ impl Client {
                 .into_iter()
                 .filter_map(|r| {
                     let index = r.transaction_index.0[0];
-                    if indices.contains(&index) {
+                    if self.indices.contains(&index) {
                         Some((r.transaction_index.0[0], r.into()))
                     } else {
                         // Otherwise, we aren't interested.
@@ -110,7 +147,7 @@ impl Client {
                 let mut result = HashMap::new();
                 let mut handles = vec![];
 
-                for index in indices {
+                for index in self.indices.iter().copied() {
                     // TODO - Call eth_getBlockTransactionCountByNumber and don't request when index > that.
                     let eth_client_clone = self.provider.clone();
                     let handle = tokio::spawn(async move {
@@ -130,7 +167,11 @@ impl Client {
                             result.insert(index, receipt.into());
                         }
                         None => {
-                            tracing::warn!("transaction at block {block}, index {index} not found");
+                            tracing::warn!(
+                                "transaction at block {}, index {} not found",
+                                self.block,
+                                index
+                            );
                         }
                     }
                 }
@@ -138,11 +179,19 @@ impl Client {
             }
         }
     }
+}
 
-    pub async fn get_erc721_uri(&self, token: &NftId) -> Result<String> {
-        let contract = ERC721Metadata::new(token.address, self.provider.clone());
+struct GetErc721Uri {
+    provider: Arc<Provider<Http>>,
+    token: NftId,
+}
+
+#[async_trait::async_trait]
+impl RetryGet<String> for GetErc721Uri {
+    async fn try_get(&self) -> Result<String> {
+        let contract = ERC721Metadata::new(self.token.address, self.provider.clone());
         contract
-            .token_uri(token.token_id.0)
+            .token_uri(self.token.token_id.0)
             .call()
             .await
             // Remove Null Bytes: Postgres can't handle them.
@@ -151,11 +200,52 @@ impl Client {
     }
 }
 
+impl Client {
+    pub fn new(url: &str) -> Result<Self> {
+        Ok(Self {
+            provider: Arc::new(Provider::<Http>::try_from(url)?),
+        })
+    }
+
+    pub async fn get_block(&self, block: u64) -> Result<Option<BlockData>> {
+        GetBlock {
+            provider: self.provider.clone(),
+            block,
+        }
+        .retry_get(3, 1)
+        .await
+    }
+
+    pub async fn get_block_receipts(
+        &self,
+        block: u64,
+        indices: HashSet<u64>,
+    ) -> Result<HashMap<u64, TxDetails>> {
+        GetBlockReceipts {
+            provider: self.provider.clone(),
+            block,
+            indices,
+        }
+        .retry_get(3, 1)
+        .await
+    }
+
+    pub async fn get_erc721_uri(&self, token: NftId) -> Result<String> {
+        GetErc721Uri {
+            provider: self.provider.clone(),
+            token,
+        }
+        .retry_get(3, 1)
+        .await
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::str::FromStr;
 
     use super::*;
+    use crate::types::U256;
     use maplit::{hashmap, hashset};
 
     static FREE_ETH_RPC: &str = "https://rpc.ankr.com/eth"; // Also supports
@@ -245,5 +335,61 @@ mod tests {
         let result = eth_client.get_block_receipts(10_000_000, indices).await;
         assert!(result.is_ok());
         assert_eq!(result.unwrap().keys().len(), 103);
+    }
+
+    #[tokio::test]
+    async fn get_erc721_uri() {
+        let eth_client = test_client();
+        let ens_token = NftId {
+            address: Address::from_str("0x57F1887A8BF19B14FC0DF6FD9B2ACC9AF147EA85").unwrap(),
+            token_id: U256::from_dec_str(
+                "64671196571681841248190411691641946869002480279128285790058847953168666315",
+            )
+            .unwrap(),
+        };
+
+        assert_eq!(
+            eth_client
+                .get_erc721_uri(ens_token)
+                .await
+                .unwrap_err()
+                .to_string(),
+            "Contract call reverted with data: 0x".to_string()
+        );
+
+        let bored_ape = NftId {
+            address: Address::from_str("0x2EE6AF0DFF3A1CE3F7E3414C52C48FD50D73691E").unwrap(),
+            token_id: U256::from(16),
+        };
+
+        assert!(eth_client.get_erc721_uri(bored_ape).await.is_ok());
+
+        let mla_field_agent = NftId {
+            address: Address::from_str("0x7A41E410BB784D9875FA14F2D7D2FA825466CDAE").unwrap(),
+            token_id: U256::from(3490),
+        };
+
+        assert_eq!(
+            eth_client
+                .get_erc721_uri(mla_field_agent)
+                .await
+                .unwrap_err()
+                .to_string(),
+            "Contract call reverted with data: 0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002f4552433732314d657461646174613a2055524920717565727920666f72206e6f6e6578697374656e7420746f6b656e0000000000000000000000000000000000".to_string()
+        );
+
+        // TODO - the above Err is a human readable message:
+        // #[derive(Debug, Deserialize)]
+        // struct Struct {
+        //     ERC721Metadata: String,
+        //     uri: String,
+        // }
+        // let hex_string = "08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002f4552433732314d657461646174613a2055524920717565727920666f72206e6f6e6578697374656e7420746f6b656e0000000000000000000000000000000000";
+        // let bytes = hex::decode(hex_string).expect("Failed to decode hex");
+        // let result: MyStruct = serde_json::from_slice(&bytes).expect("Failed to deserialize JSON");
+        // {
+        //   "ERC721Metadata": "URI query for nonexistent token",
+        //   "uri": "/"
+        // }
     }
 }
