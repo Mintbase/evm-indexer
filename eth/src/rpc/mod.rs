b0ca149fad8e526d44673ac76088be2e003b148f
Benjamin Smith <bh2smith@users.noreply.github.com>
2023-11-10 13:21:58 +0100

    Trait EthNodeRetrieving (#77)
    This PR also introduces "batch" requests to parallelize the data retrieval process.
    
    There is a follow up PR #76 that actually implements the same methods using the ethrpc library.

---
+++ eth/src/rpc/mod.rs
@@ -0,0 +1,25 @@
+pub mod ethers;
+// pub mod ethrpc;
+use crate::types::{Address, BlockData, ContractDetails, NftId};
+use anyhow::Result;
+use async_trait::async_trait;
+use std::collections::HashMap;
+
+#[async_trait]
+pub trait EthNodeReading {
+    async fn get_contract_details(
+        &self,
+        addresses: Vec<Address>,
+    ) -> HashMap<Address, ContractDetails>;
+
+    async fn get_uris(&self, token_ids: Vec<&NftId>) -> HashMap<NftId, Option<String>>;
+
+    async fn get_blocks_for_range(&self, start: u64, end: u64) -> Result<HashMap<u64, BlockData>>;
+
+    // This is waiting for: https://github.com/nlordell/ethrpc-rs/pull/3
+    // async fn get_receipts_for_range(
+    //     &self,
+    //     start: u64,
+    //     end: u64,
+    // ) -> Result<HashMap<u64, HashMap<u64, TxDetails>>>;
+}
