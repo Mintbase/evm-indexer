b8559a6fb3ccdf2b4c2644ff495e198597d021e9
Benjamin Smith <bh2smith@users.noreply.github.com>
2023-11-10 13:58:46 +0100

    Use Rpc Batching (#78)
    With node request batching we improve the performance of our event processor by ~10x. We have seen speeds as fast as 2 blocks per second (at peak of bull market).

--- eth/src/rpc/mod.rs
+++ eth/src/rpc/mod.rs
@@ -3,6 +3,7 @@ pub mod ethrpc;
 use crate::types::{Address, BlockData, ContractDetails, NftId};
 use anyhow::Result;
 use async_trait::async_trait;
+use futures::future::join;
 use std::collections::HashMap;
 
 #[async_trait]
@@ -15,6 +16,17 @@ pub trait EthNodeReading {
     async fn get_uris(&self, token_ids: Vec<&NftId>) -> HashMap<NftId, Option<String>>;
 
     async fn get_blocks_for_range(&self, start: u64, end: u64) -> Result<HashMap<u64, BlockData>>;
+
+    async fn get_uris_and_contract_details(
+        &self,
+        tokens: Vec<&NftId>,
+        addresses: Vec<Address>,
+    ) -> (
+        HashMap<NftId, Option<String>>,
+        HashMap<Address, ContractDetails>,
+    ) {
+        join(self.get_uris(tokens), self.get_contract_details(addresses)).await
+    }
 
     // This is waiting for: https://github.com/nlordell/ethrpc-rs/pull/3
     // async fn get_receipts_for_range(
