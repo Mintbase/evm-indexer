878f8215e474a1c8fd1cfbaf03a69535f7f80002
Ben Smith <bh2smith@gmail.com>
2024-02-15 12:56:22 +0100

    adding proper response types and better logging

--- metadata-retriever/src/routes/token/mod.rs
+++ metadata-retriever/src/routes/token/mod.rs
@@ -1,3 +1,4 @@
+use actix_web::HttpResponse;
 use eth::types::NftId;
 use futures::stream::{self, StreamExt};
 use serde_json::Value;
@@ -9,7 +10,7 @@ use async_trait;
 
 #[async_trait::async_trait]
 impl RequestHandler<(NftId, Option<String>)> for AppData {
-    async fn process_request(&self, tokens: &[(NftId, Option<String>)]) -> String {
+    async fn process_request(&self, tokens: &[(NftId, Option<String>)]) -> HttpResponse {
         let possible_content: Vec<Option<Value>> = stream::iter(tokens)
             .then(|(token, uri)| {
                 let app_ref = self.clone();
@@ -41,7 +42,10 @@ impl RequestHandler<(NftId, Option<String>)> for AppData {
             .lock()
             .expect("failed to lock mutex")
             .insert_metadata_batch(&updates);
-        // TODO - return status code or something.
-        return "success".to_string();
+        return HttpResponse::Ok().body(format!(
+            "added {}/{} token metadata files",
+            updates.len(),
+            tokens.len()
+        ));
     }
 }
