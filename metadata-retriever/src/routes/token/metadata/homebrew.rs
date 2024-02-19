7af559a5ad0dedc9f3d604ea61b24a075c73bc49
Ben Smith <bh2smith@gmail.com>
2024-02-19 23:14:57 +0100

    fix broken tests and ignore one

--- metadata-retriever/src/routes/token/metadata/homebrew.rs
+++ metadata-retriever/src/routes/token/metadata/homebrew.rs
@@ -111,13 +111,14 @@ mod tests {
     }
 
     #[tokio::test]
+    #[ignore = "this domain seems to be down"]
     async fn ens_override() {
         let content_result = get_fetcher()
             .get_nft_metadata(
                 NftId {
                     address: Address::from_str("0x57F1887A8BF19B14FC0DF6FD9B2ACC9AF147EA85")
                         .unwrap(),
-                    token_id: U256::from(1),
+                    token_id: U256::from(100),
                 },
                 None,
             )
@@ -138,7 +139,7 @@ mod tests {
                 .await
                 .unwrap_err()
                 .to_string(),
-            "Empty metadata url!"
+            "empty metadata url!"
         );
     }
     #[tokio::test]
