522b992d390a34a2870b6c5b42e1c1c73b7943bb
Ben Smith <bh2smith@gmail.com>
2024-02-28 15:25:18 +0100

    fix bad merge

--- metadata-retriever/src/routes/token/metadata/ipfs.rs
+++ metadata-retriever/src/routes/token/metadata/ipfs.rs
@@ -1,3 +1,8 @@
+use crate::routes::token::metadata::util::TryFromStr;
+use cid::Cid;
+use regex::Regex;
+use url::Url;
+
 pub const IPFS_GATEWAY: &str = "https://ipfs.io/ipfs/";
 pub const CID_REGEX: &str = r"(Qm[1-9A-HJ-NP-Za-km-z]{44}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})";
 
